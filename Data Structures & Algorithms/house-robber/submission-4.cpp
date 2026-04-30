c11a74a8e3ca787a1ba85ff2760d725cb05dcb38
lakshinkhurana <lakshin05@gmail.com>
2026-04-30 17:27:36 +0530

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int>dp;
+    int dfs(vector<int>& nums,int i){
+        if(i>=nums.size())return 0;
+        if(dp[i]!=-1)return dp[i];
+        dp[i]=max(dfs(nums,i+1),nums[i]+dfs(nums,i+2));
+        return dp[i];
+    }
+    int rob(vector<int>& nums){
+        dp.resize(nums.size(),-1);
+        return dfs(nums,0);
+    }
+};
